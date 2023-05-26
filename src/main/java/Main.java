6f267561c81ed468561086efbd7babf80074d5d6
kamilkubiak2210 <kamil.kubiak4@outlook.com>
2023-05-26 22:59:16 +0200

    Poprawka3.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,44 +9,40 @@ public class Main {
 
     public void run(Scanner scanner) {
         List<Integer> list = loadDataFromUser(scanner);
-        printReversedNumberList(list);
-        calculateAndPrintSum(list);
-        printTheBiggestAndTheSmallestNumber(list);
-    }
-
-    private static void printTheBiggestAndTheSmallestNumber(List<Integer> list) {
         if (!list.isEmpty()) {
-            int theSmallestNumber = list.get(0);
-            int theBiggestNumber = list.get(0);
-            for (Integer integer : list) {
-                if (integer > theBiggestNumber) {
-                    theBiggestNumber = integer;
-                }
-                if (integer < theSmallestNumber) {
-                    theSmallestNumber = integer;
-                }
-
-            }
-
-            System.out.println("Najmniejsza liczba w liście to " + theSmallestNumber);
-            System.out.println("Największa liczba w liście to " + theBiggestNumber);
+            printReversedNumberList(list);
+            calculateAndPrintSum(list);
+            printTheBiggestAndTheSmallestNumber(list);
         } else {
-            System.out.println("Lista jest pusta");
+            System.out.println("lista jest pusta");
         }
     }
 
+    private static void printTheBiggestAndTheSmallestNumber(List<Integer> list) {
+        int theSmallestNumber = list.get(0);
+        int theBiggestNumber = list.get(0);
+        for (Integer integer : list) {
+            if (integer > theBiggestNumber) {
+                theBiggestNumber = integer;
+            }
+            if (integer < theSmallestNumber) {
+                theSmallestNumber = integer;
+            }
+        }
+        System.out.println("Najmniejsza liczba w liście to " + theSmallestNumber);
+        System.out.println("Największa liczba w liście to " + theBiggestNumber);
+    }
+
     private static void calculateAndPrintSum(List<Integer> list) {
-        if (!list.isEmpty()) {
-            int addResult = 0;
-            for (int i = 0; i < list.size(); i++) {
-                Integer integer = list.get(i);
-                addResult += integer;
-                System.out.print(integer);
-                if (i < list.size() - 1) {
-                    System.out.print(" + ");
-                } else {
-                    System.out.println(" = " + addResult);
-                }
+        int addResult = 0;
+        for (int i = 0; i < list.size(); i++) {
+            Integer integer = list.get(i);
+            addResult += integer;
+            System.out.print(integer);
+            if (i < list.size() - 1) {
+                System.out.print(" + ");
+            } else {
+                System.out.println(" = " + addResult);
             }
         }
     }
@@ -59,25 +55,21 @@ public class Main {
             int i = scanner.nextInt();
             if (i < 0) {
                 userInputIsPositive = false;
-                continue;
+            } else {
+                list.add(i);
             }
-            list.add(i);
         }
         System.out.println();
         return list;
     }
 
     private static void printReversedNumberList(List<Integer> list) {
-        if (!list.isEmpty()) {
-            for (int i = list.size() - 1; i >= 0; i--) {
-                Integer integer = list.get(i);
-                System.out.print(integer);
-                if (i > 0) {
-                    System.out.print(", ");
-                }
-
+        for (int i = list.size() - 1; i >= 0; i--) {
+            Integer integer = list.get(i);
+            System.out.print(integer);
+            if (i > 0) {
+                System.out.print(", ");
             }
-
         }
         System.out.println();
     }
